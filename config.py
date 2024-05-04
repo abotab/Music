467065f1e21316bc5b262beb6a104188b2821568
كـاف، |١٤٢٨هَـ <129623634+abotab@users.noreply.github.com>
2024-05-04 09:30:54 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c42e24eaebdba59eb680f.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/c42e24eaebdba59eb680f.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/c51b5298dad62f7b1de3f.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/c51b5298dad62f7b1de3f.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/G_lRR")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Haider_1h1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/jshwiej")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/VIPD80")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5802896978").split()))
 
 
-FAILED = "https://telegra.ph/file/c42e24eaebdba59eb680f.jpg"
+FAILED = "https://telegra.ph/file/c51b5298dad62f7b1de3f.jpg"
